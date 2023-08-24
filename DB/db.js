948fe175fdbe91356d4205bf2096c32abe03ffcd
yash0922 <yashkumarpal987@gmail.com>
2023-08-24 18:00:21 +0530

    udapte sql aws server

--- DB/db.js
+++ DB/db.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('blog', 'root', 'Yash@151822', {
-  host: 'localhost',
-  dialect: 'mysql' // Specify the database type
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_NAME, process.env.PASSWORD, {
+  host:process.env.HOST,
+  dialect: 'mysql' ,// Specify the database type
+  port: 3306,
+ 
 });
+
+console.log(process.env.PASSWORD ,process.env.DATABASE,process.env.USER_NAME,process.env.HOST);
 
 // Test the connection
 (async () => {
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(cors());
 app.use(express.json());
 const sequelize = require('./DB/db')
 
+require('dotenv').config();
+
 const postRoutes = require('./Routes/post.routes');
 
 app.use('/', postRoutes);
@@ -17,7 +19,7 @@ app.use('/', postRoutes);
     console.log('Database tables synchronized');
    }) // Use { force: true } only for development
    
-  
+  // http://ec2-34-204-100-236.compute-1.amazonaws.com:8080/
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
